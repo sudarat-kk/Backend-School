ece0507b61762959495f9021269eab2e27487351
sudarat_09 <sudarat25622@gmail.com>
2026-07-15 13:54:53 +0700

    ่up

--- dbconn.ts
+++ dbconn.ts
@@ -13,7 +13,6 @@ export const conn = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    ca: process.env.DB_CA_CERT,
-    rejectUnauthorized: true, // เพิ่มส่วนนี้ทั้งหมด - Aiven บังคับใช้ SSL
+    rejectUnauthorized: false, // เพิ่มส่วนนี้ทั้งหมด - Aiven บังคับใช้ SSL
   },
 });
